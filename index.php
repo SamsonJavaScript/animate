a8f48e6665404b3cafcfd8a593a7e60454e139ea
Molodyko Ruslan <molodyko@samsonos.com>
2017-02-23 11:56:24 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+namespace samson\js\animate;
+
+/**
+ * Интерфейс для подключения модуля в ядро фреймворка SamsonPHP
+ *
+ * @package SamsonPHP
+ * @author Vitaly Iegorov <vitalyiegorov@gmail.com>
+ * @author Nikita Kotenko <nick.w2r@gmail.com>
+ * @version 0.1
+ */
+class Index extends \samson\core\CompressableExternalModule
+{	
+	/** Identifier */
+	protected $id = 'animate';
+}
